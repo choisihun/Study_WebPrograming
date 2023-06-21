1635dcb5550b9c8dab3d1d8ddb13d3f62a80b438
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-06-21 11:04:26 +0900

    ✨ :: 6월 15일 끝말잇기

--- WebPrograming/webPrograming/14week/2.js
+++ WebPrograming/webPrograming/14week/2.js
@@ -1,33 +1,68 @@
-const number = Number(prompt("몇 명이 참가하나요?"));
-const $input = document.querySelector('input');
-const $button = document.querySelector('button');
-const $word = document.querySelector('#word');
+const p = document.querySelector('#t'); //누구 차례인지
+const num = parseInt(prompt('몇 명이 참가하나요?'));
+const submitBtn = document.querySelector('#submitBtn'); //다음 버튼
+const input = document.querySelector('#input'); //입력란
+const mem = document.querySelector('#mem'); //몇명인지 화면에 띄우는 div
+const proWord = document.querySelector('#proWord');
+const orders = document.querySelector('#order');
+const wordList = document.querySelector('#wordList');
 let word;
 let newWord;
+let wordArray = [];
+mem.innerHTML = `${num}명이 참가하였습니다.`;
+
+const isDuplicate = () => {
+  if (wordArray.includes(newWord)) {
+    alert('중복된 단어가 있습니다.');
+    return false;
+  } else {
+    return true;
+  }
+};
+
 const onClickButton = () => {
-const koreanWordRegex = /^[가-힣ㄱ-ㅎㅏ-ㅣ]{2,}$/;
-const regex = /^.{2,}$/;
+  input.value = "";
+  input.focus();
 
-if (regex.test(input)) {
-    if (koreanWordRegex.test(newWord)) {
-        if (!word || word[word.length - 1] === newWord[0]) {
-                word = newWord;
-                $word.textContent = word;
-                $input.value = '';
-                $input.focus();
-            } else {
-                alert('올바르지 않은 단어입니다.');
-            }
-        } else {
-            alert('한국어만 입력하라고 이 씨');
-        }
+  if (isDuplicate()) {
+    wordArray.push(newWord);
+  }else{
+    return;
+  }
+  
+  if (!word || word[word.length - 1] === newWord[0]) {
+    word = newWord;
+    proWord.textContent = word;
+    
+    const order = Number(orders.textContent);
+    if (order + 1 > num) {
+      orders.textContent = 1;
     } else {
-        alert("두 글자 이상 입력하라고 이 씨");
+      orders.textContent = order + 1;
     }
-};
-const onInput = (event) => {
-       newWord = event.target.value;
+    proWord.innerHTML = `${newWord}<br>`;
+    wordList.innerHTML = `${wordArray.reverse()}`;
+    
+  } else {
+    alert('올바르지 않습니다.');
+  }
 };
 
-$button.addEventListener('click', onClickButton);
-$input.addEventListener('input', onInput);
+
+const onInput = (e) => {
+  const inputValue = e.target.value;
+  
+  if (!isKoreanWord(inputValue)) {
+    e.target.value = inputValue.replace(/[^\가-힣ㄱ-ㅎㅏ-ㅣ\s]/g, '');
+  }
+  
+  newWord = e.target.value;
+};
+
+const isKoreanWord = (word) => {
+  const koreanRegex = /^[가-힣ㄱ-ㅎㅏ-ㅣ\s]+$/;
+  return koreanRegex.test(word);
+};
+
+submitBtn.addEventListener("click", onClickButton);
+input.addEventListener("input", onInput);
